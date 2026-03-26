19efbf29d320166edfb2459ad9343f27c37da311
Ubuntu <ubuntu@ip-172-31-63-244.ap-northeast-2.compute.internal>
2026-03-26 14:07:54 +0000

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from "@vitejs/plugin-react";
 export default ({ mode }) => {
     const env = loadEnv(mode, process.cwd());
 
+
     return defineConfig({
         plugins: [react()],
         server: {
+            allowedHosts: ["www.topaboda.xyz", "topaboda.xyz"],
             proxy: {
                 "/api": {
                     target: env.VITE_API_URL,
